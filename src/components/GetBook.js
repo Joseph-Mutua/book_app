11f47ce08f6e4ee5705f2d2f2bf164277943458e
Joseph Mutua <Mutuaj793@gmail.com>
2022-04-27 19:00:48 +0300

    Setting page data from query in react state

--- src/components/GetBook.js
+++ src/components/GetBook.js
@@ -1,20 +1,31 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useQuery, gql } from "@apollo/client";
 import { GET_BOOK } from "../GraphQL/Queries";
 
 export const GetBook = () => {
   const { error, loading, data } = useQuery(GET_BOOK);
+  const [pages, setPages] = useState([]);
 
   useEffect(() => {
-    console.log(data);
+    if (data) {
+      console.log(data);
+      setPages(data.book.pages);
+    }
   }, [data]);
+
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
 
   return (
     <div>
-      <h1>Book</h1>
+      <h1>Page Content</h1>
+      <div>
+
+        {pages.map((page, index) => {
+          return <p key={index}>{JSON.stringify(page.content)}</p>;
+        })}
+      </div>
     </div>
   );
 };
